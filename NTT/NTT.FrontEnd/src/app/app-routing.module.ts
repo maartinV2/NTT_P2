8f51e34c0052cf20d84688cb44402beb0e300b96
MaartinV2 <maartin.venter@gmail.com>
2021-10-18 18:48:27 +0200

    finished setup with bootstrap and angular using css

--- NTT/NTT.FrontEnd/src/app/app-routing.module.ts
+++ NTT/NTT.FrontEnd/src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { ProfileComponent } from './components/profile/profile.component';
-
-
-const routes: Routes = [
-  {path:"", component: HomeComponent},
-  {path: "login", component: LoginComponent},
-  {path: "profile/user/:id", component: ProfileComponent},
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- NTT/NTT.FrontEnd/src/app/app.module.ts
+++ NTT/NTT.FrontEnd/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    ProfileComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
